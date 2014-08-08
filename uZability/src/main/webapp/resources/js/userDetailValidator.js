4fe7e60471113bd765aeb5748057f669163b1b44
victortylikov <victortylikov@gmail.com>
2014-08-08 16:38:45 +0300

    work2

--- uZability/src/main/webapp/resources/js/userDetailValidator.js
+++ uZability/src/main/webapp/resources/js/userDetailValidator.js
@@ -5,7 +5,7 @@ function check_form() {
 	var reg = new RegExp("(19[0-9]{2}|200[0-9]|201[0-5])-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])", "i");
 
 	if (reg.test(date)) {
-		alert(date);
+		alert("Данные успешно добавлены");
 		return true;
 	} else {
 		document.getElementById("errorDate").innerHTML="Дата рождения записана неверно";
