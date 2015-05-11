917fbfa812801dd8136724110b3c82d16c3fec2f
victortylikov <victortylikov@gmail.com>
2015-05-11 17:54:37 +0300

    some changes

--- uZability/src/main/webapp/resources/js/feedbackForm.js
+++ uZability/src/main/webapp/resources/js/feedbackForm.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 	$('#feedback').click(function() {
-		/*$(this).hide();*/
 		$('#div_slider_feedback').fadeIn(1000);
 	});
-	
 	$('.close_button').click(function(){
 		$('#div_slider_feedback').fadeOut(1000);
-		/*$('#feedback').show();*/
 	});
 });
